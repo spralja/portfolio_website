ef05cc0d8905e240e04db4cadda99eb51efcaa55
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-10-01 23:54:16 +0200

    Fluff

--- cv/models.py
+++ cv/models.py
@@ -1,9 +1,6 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-
-# Create your models here.
-
 class Experience(models.Model):
     authority = models.CharField(max_length=200)
     title = models.CharField(max_length=200)

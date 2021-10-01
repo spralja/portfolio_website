f42f0946a3032ea92077933ccadff63c2036857f
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-10-01 23:54:25 +0200

    Fluff fix

--- cv/models.py
+++ cv/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+
 class Experience(models.Model):
     authority = models.CharField(max_length=200)
     title = models.CharField(max_length=200)

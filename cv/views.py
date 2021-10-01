9bd28e353266e03fa90b0c0086346e91d99ae3b5
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-10-01 21:22:29 +0200

    Continuation of revert

--- cv/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpRequest
-from django.template import loader
-
-
-def index(request):
-    template = loader.get_template('cv/index')
-    return HttpRequest(template.render({}, request))

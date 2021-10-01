877956e926f1b765e5dc1b0ad520beaac248ab12
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-10-01 23:04:35 +0200

    Added rendering of experiences

--- cv/models.py
+++ cv/models.py
@@ -10,3 +10,8 @@ class Experience(models.Model):
     start_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
     description = models.TextField()
+    hide = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s - %s" % (self.authority, self.title)
+
--- cv/views.py
+++ cv/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from cv.models import Experience
+
 
 def index(request):
     template = loader.get_template('cv/index.html')
-    return HttpResponse(template.render({}, request))
+    context = {'experiences': Experience.objects.all()}
+    return HttpResponse(template.render(context, request))

9a9ffa79dee3ad8d14fa322d70ff8a518b3ddae2
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-10-01 20:21:54 +0200

    Init cv app

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Experience(models.Model):
+    authority = models.charField(max_length=200)
+    start_date = models.dateTimeField('date started')
+    end_date = models.dateTimeField('date finished')
+    description = models.

1c6cc5d54353eeb51ea7c41c286186fce6abbcd7
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-10-01 21:45:28 +0200

    Created experience Model

--- cv/models.py
+++ cv/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Experience(models.Model):
+    authority = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField()

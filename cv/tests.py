65a4a6505e9a8fe5ee4af4570e0f514ebb8fd90f
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-10-02 02:40:00 +0200

    Created tests for cv.models.Date

--- cv/tests.py
+++ cv/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+from cv.models import Date
+
+
+class DateTest(TestCase):
+    def test_compare_is_newer_test(self):
+        d1 = Date(1, 100)
+        d2 = Date(1, 10)
+        self.assertIs(d1.compare(d2) > 0, True)
+
+    def test_compare_is_older_test(self):
+        d1 = Date(1, 10)
+        d2 = Date(1, 100)
+        self.assertIs(d1.compare(d2) < 0, True)
+
+    def test_compare_is_same(self):
+        d1 = Date(1, 1)
+        d2 = Date(1, 1)
+        self.assertIs(d1.compare(d2) == 0, True)

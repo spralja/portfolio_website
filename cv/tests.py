2f9598996e3d6823a8d88091ae511d4af965ec74
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-10-02 02:50:36 +0200

    Updated tests (they were wrong before, now good)

--- cv/tests.py
+++ cv/tests.py
@@ -1,21 +1,52 @@
 from django.test import TestCase
 
 # Create your tests here.
-from cv.models import Date
+from cv.models import Date, Experience
 
+def createDate(month, year):
+    return Date(month=month, year=year)
 
 class DateTest(TestCase):
-    def test_compare_is_newer_test(self):
-        d1 = Date(1, 100)
-        d2 = Date(1, 10)
+    def test_compare_is_newer_year_test(self):
+        d1 = createDate(1, 100)
+        d2 = createDate(1, 10)
         self.assertIs(d1.compare(d2) > 0, True)
 
-    def test_compare_is_older_test(self):
-        d1 = Date(1, 10)
-        d2 = Date(1, 100)
+    def test_compare_is_older_year_test(self):
+        d1 = createDate(1, 10)
+        d2 = createDate(1, 100)
         self.assertIs(d1.compare(d2) < 0, True)
 
     def test_compare_is_same(self):
-        d1 = Date(1, 1)
-        d2 = Date(1, 1)
+        d1 = createDate(1, 1)
+        d2 = createDate(1, 1)
         self.assertIs(d1.compare(d2) == 0, True)
+
+    def test_compare_is_newer_month(self):
+        d1 = createDate(10, 1)
+        d2 = createDate(1, 1)
+        self.assertIs(d1.compare(d2) > 0, True)
+
+    def test_compare_is_older_month(self):
+        d1 = createDate(1, 1)
+        d2 = createDate(10, 1)
+        self.assertIs(d1.compare(d2) < 0, True)
+
+
+def createExperience(month1, year1, month2, year2):
+    return Experience(
+        authority="",
+        title="",
+        start_date=Date(month1, year1),
+        end_date=Date(month2, year2)
+    )
+
+
+def createExperience(mont1, year1):
+    return Experience(
+        authority="",
+        title="",
+        start_date=Date(mont1, year1),
+        end_date=None
+    )
+

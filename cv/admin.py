718f8987eb70a565ecd21519e2c66153eeaf4454
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-10-02 05:26:56 +0200

    Added new models to admin

--- cv/admin.py
+++ cv/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Experience, Date, Description
+from .models import Experience, Date, Description, Education, Project, Course
 
 admin.site.register(Experience)
 admin.site.register(Date)
 admin.site.register(Description)
+admin.site.register(Education)
+admin.site.register(Project)
+admin.site.register(Course)

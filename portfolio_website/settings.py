2132ba64f88ef0378c96a2b6bb8fac0760a479fb
Robert Spralja <robert.spralja@gmail.com>
2022-02-12 23:14:20 +0100

    made secret key secret

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f=a%67+63_8jyp0-^*5xcy#0cdmiqjb4&7nwk59dn-2w1^r^6p'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

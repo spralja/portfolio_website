22cce6b50e5981ab5be4fc7300e7694b6c632d25
Robert Spralja <56681655+spralja@users.noreply.github.com>
2022-02-22 18:05:46 +0100

    updated secrets

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASS'],
-        'HOST': os.environ['HOST'],
+        'HOST': os.environ['DB_HOST'],
     }
 }
 

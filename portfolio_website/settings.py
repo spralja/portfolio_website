cbd9c772de000db83bef494b683ec58b21cb18a8
Robert Spralja <robert.spralja@gmail.com>
2022-02-23 00:03:39 +0100

    updated DEBUG

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+
+DEBUG = (os.environ.get('DEBUG') == 'True')
 
 ALLOWED_HOSTS = ['*']
 

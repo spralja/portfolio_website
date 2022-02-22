25c9cba89f159b91010c22fcdeb2fd5e86dba2dd
Robert Spralja <robert.spralja@gmail.com>
2022-02-22 23:43:03 +0100

    Revert "made HOST a secret"
    This reverts commit ecef469e4c961b4a584240c21c329d81b55f40b7.

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ['HOST']]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -135,4 +135,4 @@ STATICFILE_STORAGE = 'whitenoise.storage.CompressManifestStaticFileStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = [os.environ['HOST']]
+CSRF_TRUSTED_ORIGINS = ['https://spralja-portfolio.azurewebsites.net']

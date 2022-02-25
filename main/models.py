822c2cc75719ba01be35764ca7d9a0c6eb59cf06
Robert Spralja <robert.spralja@gmail.com>
2022-02-25 19:37:10 +0100

    added MarkdownField

--- main/models.py
+++ main/models.py
@@ -30,7 +30,7 @@ class Project(models.Model):
     user = models.ForeignKey(User, related_name='projects', on_delete=models.CASCADE)
     title = models.CharField(max_length=DEFAULT_MAX_LENGTH)
     url = models.URLField(blank=True)
-    description = models.TextField(blank=True)
+    description = models.MarkdownField(blank=True)
 
     def __str__(self):
         return self.title

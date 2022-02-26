2fbb3e45167e6cfd784cfc2849a43716d3524788
Robert Spralja <56681655+spralja@users.noreply.github.com>
2022-02-26 03:29:37 +0100

    Revert "Revert "Revert "Staticwebsitemodel"""

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import User, Picture, Project, StaticWebsite
+from .models import User, Picture, Project
 
 admin.site.register(User)
 admin.site.register(Picture)
 admin.site.register(Project)
-admin.site.register(StaticWebsite)

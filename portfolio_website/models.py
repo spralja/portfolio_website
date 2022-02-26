8363d176effcd8a71726382b8facfe541b56d630
Robert Spralja <56681655+spralja@users.noreply.github.com>
2022-02-26 11:29:35 +0100

    Delete portfolio_website directory

--- portfolio_website/models.py
+++
@@ -1,4 +0,0 @@
-from django.db.models import *
-
-class MarkdownField(TextField):
-    pass
--- portfolio_website/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import *
-from markdown import markdown
- 
-
-class MarkdownField(CharField):
-    def to_representation(self, value):
-        return markdown.markdown(value)

9ef133d31f280094edfee6fb33a00282309bbda4
Robert Spralja <robert.spralja@gmail.com>
2022-03-06 19:08:34 +0100

    fixed contact being inacessible

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
     path('<str:name>/', views.project, name='project'),
     path('<str:name>/<str:subfile>/', views.project, name='project'),
 ]

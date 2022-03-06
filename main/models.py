cd52a45697bd635ff4c1741aaf20b6cba0871d52
Robert Spralja <robert.spralja@gmail.com>
2022-03-06 20:05:08 +0100

    added socials to template

--- main/models.py
+++ main/models.py
@@ -39,8 +39,12 @@ class Social(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='socials')
     title = models.CharField(max_length=DEFAULT_MAX_LENGTH)
     url = models.URLField()
+
     class Meta:
         unique_together = ('user', 'title')
+
+    def __str__(self):
+        return self.title
 
 
 class Project(models.Model):

3d03eb635b4982b4f16abcecf1cf4ecc63174573
Robert Spralja <robert.spralja@gmail.com>
2022-03-06 19:59:39 +0100

    added Social to admin

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import User, Picture, Project, Remote, StaticFile
+from .models import User, Picture, Social, Project, Remote, StaticFile
 
 admin.site.register(User)
 admin.site.register(Picture)
+admin.site.register(Social)
 admin.site.register(Project)
 
 

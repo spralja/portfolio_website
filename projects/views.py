f4b6685e5005f50a2a077e4267538f30ca036618
Robert Spralja <robert.spralja@gmail.com>
2022-03-08 22:56:20 +0100

    started safe deleting projects

--- projects/views.py
+++ projects/views.py
@@ -1,16 +1,3 @@
 from django.http import HttpResponse
-from .models import Image
 
-
-def image(request, image_name):
-    image = Image.objects.filter(name=image_name).first()
-    valid_image = str(image.image)
-    try:
-        with open(valid_image, "rb") as f:
-            return HttpResponse(f.read(), content_type="image/jpeg")
-    except IOError:
-        red = Image.new('RGBA', (1, 1), (255,0,0,0))
-        response = HttpResponse(content_type="image/jpeg")
-        red.save(response, "JPEG")
-        return response
         
